beec02a0571b02116e02de239e9cc7cd3ffe423e
ReactionaryFuture <ABanzhof@gmail.com>
2023-01-31 12:38:00 -0500

    Update Neural_Networks_Pt._1:_Inside_the_Black_Box.py

--- Neural_Networks_Pt._1:_Inside_the_Black_Box.py
+++ Neural_Networks_Pt._1:_Inside_the_Black_Box.py
@@ -17,25 +17,25 @@ class Neuron():
         return output
 
 
-def fprop(input, nn):
+def fprop(input):
     l1a = [n.activate(input, True) for n in nn[0]]
     l2a = [n.activate(l1a, False) for n in nn[1]]
     return l2a
 
-def plot(nn):
+def plot():
     Xs = []
     Ys = []
 
     for i in range(100):
         j = i / 100
         Xs.append(j)
-        Ys.append(fprop([j], nn))
+        Ys.append(fprop([j]))
 
     import matplotlib.pyplot as plt
     plt.scatter(Xs, Ys, color='green')
     plt.show()
 
 nn = [[Neuron([ 3.34      ], -1.43), Neuron([-3.53], 0.57)],
-      [Neuron([-1.22, -2.3],     0)                       ]]
+      [Neuron([-1.22, -2.3], -0.58)                       ]]
 
-plot(nn)
+plot()
