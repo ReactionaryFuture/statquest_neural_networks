b2eff6e4605fa7028356f0f9a52763ddbb622ebf
ReactionaryFuture <ABanzhof@gmail.com>
2023-01-31 16:30:38 -0500

    Update Neural_Networks_Pt._1:_Inside_the_Black_Box.py

--- Neural_Networks_Pt._1:_Inside_the_Black_Box.py
+++ Neural_Networks_Pt._1:_Inside_the_Black_Box.py
@@ -1,41 +1,43 @@
 import math as m
+import matplotlib.pyplot as plt
 
 
 class Neuron():
 
     def __init__(self, weight, bias):
-        self.w = weight
-        self.b = bias
+        self.weight = weight
+        self.bias   = bias
 
     def softplus(self, input):
         return m.log(1.0 + m.e ** input)
 
     def activate(self, input, act_func):
-        weighted_inputs = [i * w for i, w in zip(input, self.w)]
-        output = sum(weighted_inputs + [self.b])
+        # multiply each weight times each activation from previous layer
+        weighted_inputs = [i * w for i, w in zip(input, self.weight)]
+        # sum and add the bias
+        output = sum(weighted_inputs) + self.bias
+        # apply the softplus activation function when act_func flag set true
         if act_func: output = self.softplus(output)
         return output
 
 
 def fprop(input):
-    l1a = [n.activate(input, True) for n in nn[0]]
-    l2a = [n.activate(l1a, False) for n in nn[1]]
+    # lists of activation values for each layer of neural net
+    l1a = [n.activate(input, act_func=True ) for n in nn[0]]
+    l2a = [n.activate(  l1a, act_func=False) for n in nn[1]]
     return l2a
 
 def plot():
-    Xs = []
-    Ys = []
-
-    for i in range(100):
-        j = i / 100
-        Xs.append(j)
-        Ys.append(fprop([j]))
-
-    import matplotlib.pyplot as plt
+    # Plot x on x axis and f(x) on y axis for each 100th between 0 and 1
+    Xs = [       i/100   for i in range(100)]
+    Ys = [fprop([i/100]) for i in range(100)]
     plt.scatter(Xs, Ys, color='green')
     plt.show()
 
+# innitalize neural net with values from video
+# nn[0] is layer 1 and nn[1] is layer 2
 nn = [[Neuron([ 3.34      ], -1.43), Neuron([-3.53], 0.57)],
       [Neuron([-1.22, -2.3], -0.58)                       ]]
 
+# bam
 plot()
